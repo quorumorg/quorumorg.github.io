c85831e85a78a69abad354ffbb6eb2eba9d3325b
Kyle Stoneman <github@kylestoneman.com>
2014-12-25 21:04:03 -0500

    Slow the scrolling down
    It was going too quickly for links at the bottom of the page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   $('a').click(function(){
     $('html, body').animate({
         scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
-    }, 200);
+    }, 400);
     return false;
   });
 });
